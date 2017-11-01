f31fcd6a01d3a8002b48edd4ab0591b802def780
Tech-A19 <Tech-A19@Tech-A19s-MacBook-Air.local>
2017-11-01 00:37:13 +0000

    done the user and computer choice

--- main.js
+++ main.js
@@ -25,3 +25,23 @@ function Game(numOfRounds) {
   clearScores();
 
 }
+
+function userChoice() {
+ var playerChoice = prompt("Choose rock, paper, or scissors.");
+   if(playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
+     return playerChoice;
+   }
+  alert("You typed something else or did not spell your choice correctly please try again!");
+  playerGuess();
+}
+
+function computerChoice() {
+  var choice = Math.random();
+  if(choice < 0.34) {
+    return "rock";
+  }
+  if(choice <= 0.67) {
+    return "paper";
+  }
+  return "scissors";
+}
